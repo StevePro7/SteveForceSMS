9eeb785eb6d57c5e45110d1fb8ffc65e217509c3
StevePro7 <steveprozune@hotmail.com>
2020-04-06 20:38:47 +0100

    no message

--- dev/build.bat
+++ dev/build.bat
@@ -1,79 +1,58 @@
 @echo off
-cls
 
-REM Banks conversion
-REM cd Banks
-REM folder2c bank2 bank2 2
-REM folder2c bank3 bank3 3
-REM folder2c bank4 bank4 4
-REM folder2c bank5 bank5 5
-REM folder2c bank6 bank6 6
-REM folder2c bank7 bank7 7
-REM folder2c bank8 bank8 8
-REM folder2c bank9 bank9 9
-REM folder2c bank10 bank10 10
-REM folder2c bank11 bank11 11
-REM folder2c bank12 bank12 12
-REM folder2c bank13 bank13 13
-REM folder2c bank14 bank14 14
-REM folder2c bank15 bank15 15
+:: Time build START
+:: Reference: https://stackoverflow.com/questions/673523/how-do-i-measure-execution-time-of-a-command-on-the-windows-command-line
+set _time=%time: =0%
+set /a _hours=100%_time:~0,2%%%100,_min=100%_time:~3,2%%%100,_sec=100%_time:~6,2%%%100,_cs=%_time:~9,2%
+set /a _started=_hours*60*60*100+_min*60*100+_sec*100+_cs
 
-REM Compile banks
-REM sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK2 bank2.c
-REM sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK3 bank3.c
-REM sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK4 bank4.c
-REM sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK5 bank5.c
-REM sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK6 bank6.c
-REM sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK7 bank7.c
-REM sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK8 bank8.c
-REM sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK9 bank9.c
-REM sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK10 bank10.c
-REM sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK11 bank11.c
-REM sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK12 bank12.c
-REM sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK13 bank13.c
-REM sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK14 bank14.c
-REM sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK15 fixedbank.c
-REM cd ..
 
-REM Build main
-sdcc -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 main.c
+:: Compile
+cd banks
+::sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-size --constseg BANK15 fixedbank.c
+cd ..
 
-REM Link files
-sdcc -o output.ihx --Werror --opt-code-speed -mz80 --no-std-crt0 --data-loc 0xC000 ^
+
+cd devkit
+::sdcc -c -mz80 --opt-code-size --peep-file ..\peep-rules.txt --std-c99 _sms_manager.c
+::sdcc -c -mz80 --opt-code-size --peep-file ..\peep-rules.txt --std-c99 _snd_manager.c
+cd ..
+
+
+:: echo Build main
+sdcc -c -mz80 --opt-code-size --peep-file peep-rules.txt --std-c99 main.c
+
+
+:: Time build -END-
+set _time=%time: =0%
+set /a _hours=100%_time:~0,2%%%100,_min=100%_time:~3,2%%%100,_sec=100%_time:~6,2%%%100,_cs=%_time:~9,2%
+set /a _duration=_hours*60*60*100+_min*60*100+_sec*100+_cs-_started
+set /a _hours=_duration/60/60/100,_min=100+_duration/60/100%%60,_sec=100+(_duration/100%%60%%60),_cs=100+_duration%%100
+echo.
+echo Time taken: %_sec:~-2%.%_cs:~-2% secs
+echo.
+
+
+:: Link
+sdcc -o output.ihx -mz80 --no-std-crt0 --data-loc 0xC000 ^
 ..\crt0\crt0_sms.rel main.rel ^
+-Wl-b_BANK2=0x8000  -Wl-b_BANK3=0x8000  -Wl-b_BANK4=0x8000  -Wl-b_BANK5=0x8000 ^
+-Wl-b_BANK6=0x8000  -Wl-b_BANK7=0x8000  -Wl-b_BANK8=0x8000  -Wl-b_BANK9=0x8000 ^
+-Wl-b_BANK10=0x8000 -Wl-b_BANK11=0x8000 -Wl-b_BANK12=0x8000 -Wl-b_BANK13=0x8000 ^
+-Wl-b_BANK14=0x8000 -Wl-b_BANK15=0x8000 ^
 ..\lib\SMSlib.lib ^
 ..\lib\PSGlib.rel
 
-REM Binary output
+:: Execute
 ihx2sms output.ihx output.sms
 
 
-REM echo Copy output
-copy output.sms ..\asm
-REM copy output.sms ..\SteveForce.sms
+
+if exist "*.ihx" del "*.ihx" > nul; if exist "*.lk"  del "*.lk"  > nul; if exist "*.asm" del "*.asm" > nul
+if exist "*.noi" del "*.noi" > nul; if exist "*.sym" del "*.sym" > nul; if exist "*.lst" del "*.lst" > nul
 
 
-REM echo Disassemble output
-cd ..\asm
-smsexamine output.sms
-cd ..\dev
-
-
-REM echo Delete
-REM cd Banks
-REM del *.asm > nul
-REM del *.lst > nul
-REM del *.rel > nul
-REM del *.sym > nul
-REM cd ..
-
-del *.asm > nul
-del *.ihx > nul
-del *.lk > nul
-del *.lst > nul
-del *.map > nul
-del *.noi > nul
-del *.rel > nul
-del *.sym > nul
-
+:: Run
+::java -jar C:\SEGA\Emulicious\Emulicious.jar output.sms
+::C:\SEGA\meka\mekaw.exe output.sms
 output.sms
--- dev/main.c
+++ dev/main.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 void main()
-{	
+{
+	devkit_SMS_setSpritePaletteColor( 0, 3, 3, 3 );
 	while(1)
 	{
+		devkit_SMS_waitForVBlank();
 	}
 }
