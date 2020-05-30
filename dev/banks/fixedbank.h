124226d606702013185f76cdfcee31c5a19a49d1
StevePro7 <steveprozune@hotmail.com>
2020-05-30 21:33:01 +0100

    no message

--- dev/banks/fixedbank.h
+++ dev/banks/fixedbank.h
@@ -245,8 +245,6 @@ extern const unsigned char enemiesheight[];
 extern const unsigned char enemiesenergy[];
 
 
-
-
 extern const unsigned char	intro1_psg[];
 #define				intro1_psg_size 2369
 #define				intro1_psg_bank 15
